edea5119d3f01da8c34c5a50277df3824aea2cf4
GenBrg <2322168080@qq.com>
2021-07-14 19:11:09 +0800

    Resource spawner 10%

--- Assets/Scripts/ResourceSpawner.cs
+++ Assets/Scripts/ResourceSpawner.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class ResourceSpawner : MonoBehaviour
 {
+    public List<Vector3> spawnPoints;
+    
+
+    void Spawn()
+    {
+        foreach (Vector3 spawnPoint in spawnPoints)
+        {
+
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        TimeManager.Instance.RegisterRoutineStartsAt(0, 6, 0, () =>
+        {
+
+
+        });
     }
 
     // Update is called once per frame
